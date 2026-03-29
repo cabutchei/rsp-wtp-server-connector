f62bbc5be1fd6a88b5ce98a1f919019019fdd223
cabutchei <luropa_paz@hotmail.com>
2026-03-29 20:30:42 -0300

    package script

--- build/package.js
+++ build/package.js
@@ -1,31 +1,64 @@
+const path = require('path');
 const fs = require('fs-extra');
 const download = require('download');
 const decompress = require('decompress');
 
-const qualifier = process.env.RSP_QUALIFIER === 'snapshots' ? 'snapshots' : 'stable';
-console.log('The qualifier is ' + qualifier);
-const RSP_PREFIX = `http://download.jboss.org/jbosstools/adapters/${qualifier}/rsp-server`;
-const RSP_SERVER_LATEST = 'LATEST';
-const RSP_SERVER_LATEST_URL = 'https://raw.githubusercontent.com/redhat-developer/rsp-server/refs/heads/master/LATEST';
+const connectorRoot = path.resolve(__dirname, '..');
+const targetServerDir = path.join(connectorRoot, 'server');
+const defaultLatestUrl = 'https://raw.githubusercontent.com/cabutchei/rsp-wtp-server/refs/heads/master/LATEST';
+const latestUrl = process.env.RSP_SERVER_LATEST_URL || defaultLatestUrl;
+const latestVersionKey = 'com.github.cabutchei.rsp.distribution.latest.version';
+const latestArchiveUrlKey = 'com.github.cabutchei.rsp.distribution.latest.url';
 
-
-function clean() {
-    return Promise.resolve();
+function parseLatest(text) {
+    const values = new Map();
+    for (const rawLine of text.split(/\r?\n/)) {
+        const line = rawLine.trim();
+        if (!line || line.startsWith('#')) {
+            continue;
+        }
+        const separator = line.indexOf('=');
+        if (separator === -1) {
+            continue;
+        }
+        const key = line.slice(0, separator).trim();
+        const value = line.slice(separator + 1).trim();
+        values.set(key, value);
+    }
+    const version = values.get(latestVersionKey);
+    const archiveUrl = values.get(latestArchiveUrlKey);
+    if (!archiveUrl) {
+        throw new Error(`Missing '${latestArchiveUrlKey}' in ${latestUrl}`);
+    }
+    return { version, archiveUrl };
 }
 
-Promise.resolve()
-    .then(clean)
-    .then(()=> console.log(`Downloading ${RSP_SERVER_LATEST_URL}`))
-    .then(()=> download(RSP_SERVER_LATEST_URL, './'))
-    .then(()=> fs.readFileSync(RSP_SERVER_LATEST).toString())
-    .then((v)=> { console.log(v); return v;})
-    .then((v)=> v.split('\n')[1])
-    .then((v)=> v.split('=')[1])
-    .then((v)=> {console.log('downloading ' + v); return v;})
-    .then((v)=> {
-        download(v, './')
-            .then(()=>v.split('/').reverse()[0])
-            .then((x)=>decompress(x, './server', { strip: 1}))
-            .catch((err)=>{throw err;});
-    })
-    .catch((err)=>{ throw err; });
+function archiveFileName(archiveUrl) {
+    return path.basename(new URL(archiveUrl).pathname);
+}
+
+async function main() {
+    console.log(`Downloading ${latestUrl}`);
+    const latestBuffer = await download(latestUrl);
+    const latestText = latestBuffer.toString('utf8');
+
+    const latest = parseLatest(latestText);
+    console.log(`Resolved server bundle${latest.version ? ` ${latest.version}` : ''} from ${latest.archiveUrl}`);
+
+    await fs.remove(targetServerDir);
+    await fs.ensureDir(targetServerDir);
+
+    const archiveUrl = latest.archiveUrl;
+    const archiveName = archiveFileName(archiveUrl);
+    console.log(`Downloading ${archiveUrl}`);
+    await download(archiveUrl, connectorRoot);
+
+    console.log(`Extracting ${archiveName} -> ${targetServerDir}`);
+    await decompress(path.join(connectorRoot, archiveName), targetServerDir, { strip: 1 });
+    await fs.remove(path.join(connectorRoot, archiveName));
+}
+
+main().catch((err) => {
+    console.error(err && err.stack ? err.stack : err);
+    process.exit(1);
+});
